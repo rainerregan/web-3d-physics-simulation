7262515acef9c3e3bb6d0c5e2867c82511149cfe
Rainer Regan (Exacode) <rainercoc@gmail.com>
2024-08-21 08:32:21 +0700

    feat: Create Simple Box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='w-full h-screen'>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" wireframe />
+        </mesh>
+      </Canvas>
+    </div>
   )
 }
 
